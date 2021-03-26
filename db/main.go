9387af2ce30212248be0e970505b06068de63913
hem-shrestha <62273037+hem-shrestha@users.noreply.github.com>
2021-03-26 11:51:59 +0545

    db changed to sqlite

--- db/main.go
+++ db/main.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/jinzhu/gorm"
 	"os"
 )
@@ -10,22 +10,10 @@ import (
 var DB *gorm.DB
 
 func SetupDB() *gorm.DB {
-
-	//db config vars
-	var dbHost string = os.Getenv("DB_HOST")
-	var dbName string = os.Getenv("DB_NAME")
-	var dbUser string = os.Getenv("DB_USERNAME")
-	var dbPassword string = os.Getenv("DB_PASSWORD")
-
 	//connect to db
-	db, dbError := gorm.Open("mysql", dbUser+":"+ dbPassword +"@tcp(" + dbHost+ ":3306)/"+ dbName + "?charset=utf8&parseTime=True&loc=Local")
+	db, dbError := gorm.Open("sqlite3", "data.db")
 	if dbError != nil {
 		panic("Failed to connect to database")
 	}
-
-	//fix for connection timeout
-	//see: https://github.com/go-sql-driver/mysql/issues/257
-	db.DB().SetMaxIdleConns(0)
-
 	return db
 }
